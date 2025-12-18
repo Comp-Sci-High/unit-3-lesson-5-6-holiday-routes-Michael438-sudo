3adc3a62fac81b16a4ffacda6b70345e9dec47c4
Michael438-sudo <michael.robinson26@compscihigh.org>
2025-12-18 21:48:09 +0000

    Completed

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 })
 
+app.get("/chinese-new-year", (req, res) => {
+  res.sendFile(__dirname + "/public/chinese-new-year.html");
+})
+
+app.get("/japanese-new-year", (req, res) => {
+  res.sendFile(__dirname + "/public/japanese-new-year.html");
+})
+
+app.get("/exotic-christmas", (req, res) => {
+  res.sendFile(__dirname + "/public/exotic-christmas.html");
+})
+
 app.listen(3000, () => {
   console.log(`Holiday Server is Running!`)
 });
